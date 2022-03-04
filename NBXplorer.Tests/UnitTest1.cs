379e7a38b73ca3b70fc0b31575d3973152360510
nicolas.dorier <nicolas.dorier@gmail.com>
2022-03-04 21:42:15 +0900

    working

--- NBXplorer.Tests/UnitTest1.cs
+++ NBXplorer.Tests/UnitTest1.cs
@@ -1115,9 +1115,9 @@ namespace NBXplorer.Tests
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 				//   0/0 and 0/2 used
-				tester.SendToAddressAsync(a1.ScriptPubKey, Money.Coins(1.0m));
-				tester.SendToAddressAsync(a3.ScriptPubKey, Money.Coins(1.0m));
-				var txId = tester.SendToAddressAsync(a4.ScriptPubKey, Money.Coins(1.0m)).GetAwaiter().GetResult();
+				tester.SendToAddress(a1.ScriptPubKey, Money.Coins(1.0m));
+				tester.SendToAddress(a3.ScriptPubKey, Money.Coins(1.0m));
+				var txId = tester.SendToAddress(a4.ScriptPubKey, Money.Coins(1.0m));
 				tester.Notifications.WaitForTransaction(bob, txId);
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 				a1 = tester.Client.GetUnused(bob, DerivationFeature.Deposit, 0);
