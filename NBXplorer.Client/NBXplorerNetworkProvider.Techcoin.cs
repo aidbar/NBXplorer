a50f12feef216b59ac04d2965aaf75f99479c9be
aidbar <aide.barniskyte@tuni.fi>
2022-03-21 13:03:33 +0200

    NBXexplorerNetworkProvider for TLC

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Techcoin.cs
@@ -0,0 +1,24 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBXplorer
+{
+    public partial class NBXplorerNetworkProvider
+    {
+		private void InitTechcoin(ChainName networkType)
+		{
+			Add(new NBXplorerNetwork(NBitcoin.Altcoins.Techcoin.Instance, networkType)
+			{
+				MinRPCVersion = 140200,
+				CoinType = networkType == ChainName.Mainnet ? new KeyPath("2'") : new KeyPath("1'"),
+			});
+		}
+
+		public NBXplorerNetwork GetTLC()
+		{
+			return GetFromCryptoCode(NBitcoin.Altcoins.Techcoin.Instance.CryptoCode);
+		}
+	}
+}
